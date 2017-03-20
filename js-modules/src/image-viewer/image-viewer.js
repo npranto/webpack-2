45f3a68ed4dbe45871a1237948238c9bb0446b81
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-03-20 14:13:54 -0400

    adds loaders for compressing and bundling large images

--- js-modules/src/image-viewer/image-viewer.js
+++ js-modules/src/image-viewer/image-viewer.js
@@ -1,6 +1,6 @@
-// import portrait from './../../assets/images/portraits/portrait-black-and-white-close.jpeg';
-// import big from './../../assets/images/lorem-pixel-photos/big.jpeg';
-// import small from './../../assets/images/lorem-pixel-photos/small.jpeg';
+import portrait from './../../assets/images/portraits/portrait-black-and-white-close.jpeg';
+import big from './../../assets/images/lorem-pixel-photos/big.jpeg';
+import small from './../../assets/images/lorem-pixel-photos/small.jpeg';
 
 import './image-viewer.css';
 
@@ -9,10 +9,10 @@ const image = document.createElement('img');
 image['src'] = 'http://lorempixel.com/400/400';
 document.body.appendChild(image);
 
-// const smallImage = document.createElement('img');
-// image['src'] = small;
-// document.body.appendChild(smallImage);
-//
-// const bigImage = document.createElement('img');
-// image['src'] = big;
-// document.body.appendChild(bigImage);
+const smallImage = document.createElement('img');
+smallImage['src'] = small;
+document.body.appendChild(smallImage);
+
+const bigImage = document.createElement('img');
+bigImage['src'] = big;
+document.body.appendChild(bigImage);
